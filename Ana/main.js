012358032b11d3623ecd2f034711f56965529709
ANA <anamaldo82@gmail.com>
2023-10-30 21:25:55 -0300

    corregido mayuscula

--- Ana/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.banner');
-  ScrollReveal().reveal('.cards', { delay: 500 });
-  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
